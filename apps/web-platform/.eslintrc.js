7cdfa464e636ea98fe07e556a34d0d52d6915ebb
garrett-mccurrach <garrett.mccurrach@eagles.oc.edu>
2025-06-26 14:52:10 -0500

    Simplify ESLint config to remove TypeScript rules that require additional plugins

--- apps/web-platform/.eslintrc.js
+++ apps/web-platform/.eslintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
   extends: ['next/core-web-vitals'],
   rules: {
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-explicit-any': 'warn',
     'prefer-const': 'error',
     'no-var': 'error',
   },
